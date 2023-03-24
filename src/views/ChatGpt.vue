2f41d98ef8605a46347b2340a44aec2445d792e1
MOROZUMI, Hiroyuki <113565981+h-morozumi@users.noreply.github.com>
2023-03-24 09:02:45 +0000

    add markdown view

--- src/views/ChatGpt.vue
+++ src/views/ChatGpt.vue
@@ -2,16 +2,53 @@
     <div class="chatgpt">
       <h1>This is an ChatGPT page</h1>
     </div>
+    <div class="container">
+        <vue-markdown-it :source="source" />
+    </div>
 </template>
 <script>
+import VueMarkdownIt from 'vue3-markdown-it';
+import 'highlight.js/styles/monokai.css';
+
+const source = `
+# Hello
+
+- apple
+- banana
+
+~~打ち消し~~
+
+> aaaaaa
+> bbbbb
+> cccc
+
+[Qiita](http://qiita.com)
+`
+
 export default {
   name: 'ChatGpt',
-  components: {},
+  components: {
+    VueMarkdownIt
+  },
   data() {
-    return {};
+    return {
+        source: source
+    };
   },
   computed: {},
   mounted() {},
   methods: {},
 };
-</script>  
+</script>
+<style>
+.container {
+  margin: 0 auto;
+  margin-top: 200px;
+  min-height: 100vh;
+  width: 500px;
+  justify-content: center;
+  align-items: center;
+  text-align: left;
+  background-color: #c0c0c0;
+}
+</style>
