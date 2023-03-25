7c322404301ca12034e4a01e124c17f8b1f36865
MOROZUMI, Hiroyuki <113565981+h-morozumi@users.noreply.github.com>
2023-03-25 13:44:16 +0000

    UI Frameworkを導入

--- src/main.js
+++ src/main.js
@@ -2,4 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
+
+const app = createApp(App);
+app.use(router);
+app.use(vuetify).mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <v-btn elevation="2">test</v-btn>
   </div>
 </template>
